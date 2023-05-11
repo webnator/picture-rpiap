ff754281b690636e076151a145e47b724f147c1d
webnator <webnator@gmail.com>
2023-05-11 13:50:10 +0200

    ADD: Added reset button

---
+++ resetButton.js
@@ -0,0 +1,19 @@
+const rpio = require('rpio');
+
+rpio.open(10, rpio.INPUT, rpio.PULL_UP);
+
+function pollcb(pin) {
+  /*
+   * Wait for a small period of time to avoid rapid changes which
+   * can't all be caught with the 1ms polling frequency.  If the
+   * pin is no longer down after the wait then ignore it.
+   */
+  rpio.sleep(5);
+
+  if (rpio.read(pin))
+    return;
+
+  console.log('Button pressed on pin P%d', pin);
+}
+
+rpio.poll(10, pollcb, rpio.POLL_LOW);
