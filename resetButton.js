6d9e2bb1a9ea875f3b9f8e08dd9f26a722841b34
webnator <webnator@gmail.com>
2023-05-11 13:54:14 +0200

    ADD: keep trying

--- resetButton.js
+++ resetButton.js
@@ -3,6 +3,7 @@ const rpio = require('rpio');
 rpio.open(10, rpio.INPUT, rpio.PULL_UP);
 
 function pollcb(pin) {
+  console.log('Detected');
   /*
    * Wait for a small period of time to avoid rapid changes which
    * can't all be caught with the 1ms polling frequency.  If the
@@ -10,10 +11,15 @@ function pollcb(pin) {
    */
   rpio.sleep(5);
 
-  if (rpio.read(pin))
+  if (rpio.read(pin)) {
+    console.log('Pull retired');
     return;
+  }
+    
 
   console.log('Button pressed on pin P%d', pin);
 }
 
 rpio.poll(10, pollcb, rpio.POLL_LOW);
+
+console.log('Started!');
