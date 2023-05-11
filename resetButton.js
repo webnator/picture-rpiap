a6a4d1fa35c9487844b1fbb703a4369cc752422c
webnator <webnator@gmail.com>
2023-05-11 15:48:46 +0200

    ADD: keep trying

--- resetButton.js
+++ resetButton.js
@@ -1,21 +1,21 @@
 const rpio = require('rpio');
-const { write_template_to_file } = require('./app/template_writer');
-const exec = require("child_process").exec;
+const fs = require('fs');
+const exec = require('child_process').exec;
 
 rpio.open(10, rpio.INPUT, rpio.PULL_DOWN);
-
-
 
 function pollcb(pin) {
   rpio.msleep(20);
 
   if (rpio.read(pin)) {
-    write_template_to_file(
-      "./assets/etc/wpa_supplicant/empty_wpa_supplicant.conf.template",
-      "/etc/wpa_supplicant/wpa_supplicant.conf",
-      {}, () => null);
+    console.log('Starting reset');
     
+    const file_txt = fs.readFileSync('./assets/etc/wpa_supplicant/empty_wpa_supplicant.conf.template', { encoding: 'utf8' });
+    fs.writeFileSync(file_name, file_txt);
+
+    console.log('Wifi reset successfuly. Rebooting');
     exec('reboot');
+
     process.exit(0);
   }
 }
@@ -23,4 +23,3 @@ function pollcb(pin) {
 rpio.poll(10, pollcb, rpio.POLL_BOTH);
 
 console.log('Started!');
-console.log('Pint %d, is now %d', 12, rpio.read(12));
